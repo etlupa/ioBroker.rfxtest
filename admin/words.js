ddb154dfe9a5b604da9757a705bcffc297f230ab
etlupa <etlupa@hotmail.com>
2020-04-08 10:35:19 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"rfxtest adapter settings": {
+		"en": "Adapter settings for rfxtest",
+		"de": "Adaptereinstellungen für rfxtest",
+		"ru": "Настройки адаптера для rfxtest",
+		"pt": "Configurações do adaptador para rfxtest",
+		"nl": "Adapterinstellingen voor rfxtest",
+		"fr": "Paramètres d'adaptateur pour rfxtest",
+		"it": "Impostazioni dell'adattatore per rfxtest",
+		"es": "Ajustes del adaptador para rfxtest",
+		"pl": "Ustawienia adaptera dla rfxtest",
+		"zh-cn": "rfxtest的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
